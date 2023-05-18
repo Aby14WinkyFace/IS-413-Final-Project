e5af381ee4374a4f34633e7f4b4c96957c91bbce
Aby14WinkyFace <39811722+Aby14WinkyFace@users.noreply.github.com>
2023-05-18 00:38:53 -0400

    Update ContactController.java

--- ContactController.java
+++ ContactController.java
@@ -1,26 +1,42 @@
 package com.example.mvcfinal_2023;
 
-public class ContactController implements Listener{
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableView;
 
-    private ContactView contactView;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+public class ContactController {
     private ContactModel contactModel;
-    public ContactController(ContactView contactView, ContactModel contactModel){
-        this.contactView = contactView;
-        this.contactModel = contactModel;
-        //contactView.setListener(this);
-        //contactView.start();
+    private TableView<PersonEntry> tableView;
 
+    public ContactController(ContactModel contactModel, TableView<PersonEntry> tableView) {
+        this.contactModel = contactModel;
+        this.tableView = tableView;
+        bindTableView();
     }
 
+    private void bindTableView() {
+        ObservableList<PersonEntry> contactList = FXCollections.observableArrayList(contactModel.getContacts());
+        tableView.setItems(contactList);
+    }
 
+    public void addContact(PersonEntry contact) {
+        contactModel.addContact(contact);
+    }
 
+    public void removeContact(PersonEntry contact) {
+        contactModel.removeContact(contact);
+    }
 
-
-
-    @Override
-    public void entryEntered(String name, String phone, String email) {
-        contactModel.changeName(name);
-        contactModel.changePhone(phone);
-        contactModel.changeEmail(email);
+    public void saveContactsToFile(String filePath) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(filePath))) {
+            for (PersonEntry contact : contactModel.getContacts()) {
+                writer.println(contact.getName() + "," + contact.getEmail() + "," + contact.getPhoneNumber());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
