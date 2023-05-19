0a7765fcdfa5da6b5292b40b0c126c89b50a3e20
Aby14WinkyFace <39811722+Aby14WinkyFace@users.noreply.github.com>
2023-05-19 03:34:42 -0400

    Update ContactController.java

--- ContactController.java
+++ ContactController.java
@@ -6,14 +6,24 @@ import javafx.scene.control.TableView;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ContactController {
     private ContactModel contactModel;
     private TableView<PersonEntry> tableView;
+    private ContactView contactView;
 
-    public ContactController(ContactModel contactModel, TableView<PersonEntry> tableView) {
+    private String filename = "src/main/resources/project/final_project/SavedContacts.txt";
+
+    public ContactController(ContactModel contactModel, TableView<PersonEntry> tableView, ContactView contactView) {
         this.contactModel = contactModel;
         this.tableView = tableView;
+        this.contactView = contactView;
         bindTableView();
+        setupButtonHandlers();
     }
 
     private void bindTableView() {
@@ -21,21 +31,101 @@ public class ContactController {
         tableView.setItems(contactList);
     }
 
-    public void addContact(PersonEntry contact) {
-        contactModel.addContact(contact);
-    }
-
-    public void removeContact(PersonEntry contact) {
-        contactModel.removeContact(contact);
-    }
-
     public void saveContactsToFile(String filePath) {
         try (PrintWriter writer = new PrintWriter(new FileWriter(filePath))) {
             for (PersonEntry contact : contactModel.getContacts()) {
-                writer.println(contact.getName() + "," + contact.getEmail() + "," + contact.getPhoneNumber() + "," + contact.getEmailS());
+                writer.println(contact.getFName() + "," +
+                                contact.getLName() + "," +
+                                contact.getPhoneNumber() + "," +
+                                contact.getPriEmail()+ "," +
+                                contact.getSecEmail());
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    void setupButtonHandlers() {
+        contactView.getBtnAdd().setOnAction(e -> {
+            String firstName = contactView.tfFirstName.getText();
+            String lastName = contactView.tfLastName.getText();
+            long phoneNumber = Long.parseLong(contactView.tfPhoneNumber.getText());
+            String primaryEmail = contactView.tfFirstEmail.getText();
+            String secondaryEmail = contactView.tfSecondEmail.getText();
+
+            PersonEntry newEntry = new PersonEntry(firstName, lastName, phoneNumber, primaryEmail, secondaryEmail);
+
+            contactModel.addContact(newEntry);
+
+            contactView.tfFirstName.clear();
+            contactView.tfLastName.clear();
+            contactView.tfPhoneNumber.clear();
+            contactView.tfFirstEmail.clear();
+            contactView.tfSecondEmail.clear();
+
+            tableView.setItems(FXCollections.observableArrayList(contactModel.contacts));
+            tableView.refresh();
+        });
+
+        contactView.getBtnEdit().setOnAction(e -> {
+            PersonEntry selectedEntry = tableView.getSelectionModel().getSelectedItem();
+
+            if (selectedEntry != null) {
+
+                String firstName = contactView.tfFirstName.getText();
+                String lastName = contactView.tfLastName.getText();
+                long phoneNumber = Long.parseLong(contactView.tfPhoneNumber.getText());
+                String primaryEmail = contactView.tfFirstEmail.getText();
+                String secondaryEmail = contactView.tfSecondEmail.getText();
+
+                contactModel.editContact(selectedEntry, firstName, lastName, phoneNumber, primaryEmail, secondaryEmail);
+
+                contactView.tfFirstName.clear();
+                contactView.tfLastName.clear();
+                contactView.tfPhoneNumber.clear();
+                contactView.tfFirstEmail.clear();
+                contactView.tfSecondEmail.clear();
+
+                tableView.refresh();
+            }
+        });
+
+        contactView.getBtnDelete().setOnAction(e -> {
+
+            PersonEntry selectedEntry = tableView.getSelectionModel().getSelectedItem();
+            if (selectedEntry != null) {
+                contactModel.removeContact(selectedEntry);
+            }
+            tableView.setItems(FXCollections.observableArrayList(contactModel.contacts));
+            tableView.refresh();
+        });
+
+        contactView.getBtnSave().setOnAction(e -> {
+            saveContactsToFile(filename);
+        });
+    }
+
+    List<PersonEntry> readContactsFromFile() {
+        List<PersonEntry> loadContacts = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length == 5) {
+                    String firstName = parts[0].trim();
+                    String lastName = parts[1].trim();
+                    long phoneNumber = Long.parseLong(parts[2].trim());
+                    String primaryEmail = parts[3].trim();
+                    String secondaryEmail = parts[4].trim();
+
+                    PersonEntry newEntry = new PersonEntry(firstName, lastName, phoneNumber, primaryEmail, secondaryEmail);
+                    loadContacts.add(newEntry);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return loadContacts;
     }
 }
