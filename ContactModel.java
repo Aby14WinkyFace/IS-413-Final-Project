264c23474b580c60f90167c675369cdd05513247
Aby14WinkyFace <39811722+Aby14WinkyFace@users.noreply.github.com>
2023-05-19 03:35:23 -0400

    Update ContactModel.java

--- ContactModel.java
+++ ContactModel.java
@@ -1,11 +1,10 @@
 package com.example.mvcfinal_2023;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class ContactModel {
-    private List<PersonEntry> contacts;
+    List<PersonEntry> contacts;
 
     public ContactModel() {
         contacts = new ArrayList<>();
@@ -18,14 +17,15 @@ public class ContactModel {
     public void removeContact(PersonEntry contact) {
         contacts.remove(contact);
     }
-    public void editContact(PersonEntry contact, String newName, String newEmail,
-                            String newPhoneNumber, String newEmailS) {
+    public void editContact(PersonEntry contact, String newFirstName, String newLastName,
+                            Long newPhoneNumber, String newPriEmail, String newSecEmail ) {
         int index = contacts.indexOf(contact);
         if (index != -1) {
-            contact.setName(newName);
-            contact.setEmail(newEmail);
+            contact.setFName(newFirstName);
+            contact.setLName(newLastName);
+            contact.setPriEmail(newPriEmail);
+            contact.setSecEmail(newSecEmail);
             contact.setPhoneNumber(newPhoneNumber);
-            contact.setEmailS(newEmailS);
             contacts.set(index, contact);
         }
     }
